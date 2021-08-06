646a2164607db6ed6b1fefc586f6b5fe7f2ff445
Pieter van Ginkel <pvginkel@gmail.com>
2021-08-06 09:53:54 +0200

    Initial commit.

---
+++ CashflowINGSpaarCSVConversie/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashflowINGSpaarCSVConversie
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sourceFileName = args[0];
+            string targetFileName = sourceFileName;
+            if (args.Length > 1)
+                targetFileName = args[1];
+
+            var source = ReadCsv(sourceFileName);
+
+            var target = new List<List<string>>
+            {
+                new List<string>
+                {
+                    "Datum",
+                    "Naam / Omschrijving",
+                    "Rekening",
+                    "Tegenrekening",
+                    "Code",
+                    "Af Bij",
+                    "Bedrag (EUR)",
+                    "Mutatiesoort",
+                    "Mededelingen"
+                }
+            };
+
+
+            foreach (var line in source.Skip(1))
+            {
+                target.Add(new List<string>
+                {
+                    line[0].Replace("-", ""), // Datum -> Datum
+                    line[1], // Omschrijving -> Naam / Omschrijving
+                    line[2], // Rekening -> Rekening
+                    line[4], // Tegenrekening -> Tegenrekening
+                    "", // _ -> Code
+                    line[5], // Ad Bij -> Af Bij
+                    line[6], // Bedrag -> Bedrag (EUR)
+                    line[8], // Mutatiesoort -> Mutatiesoort
+                    line[9] // Mededeling -> Mededeling
+                });
+            }
+
+            WriteCsv(targetFileName, target);
+        }
+
+        private static List<List<string>> ReadCsv(string fileName)
+        {
+            return File.ReadAllLines(fileName)
+                .Select(p => ParseCsv(p).ToList())
+                .ToList();
+        }
+
+        private static IEnumerable<string> ParseCsv(string line)
+        {
+            bool inQuotes = false;
+            char lastChar = (char)0;
+            var sb = new StringBuilder();
+
+            foreach (char c in line)
+            {
+                if (c == '"')
+                {
+                    if (inQuotes)
+                    {
+                        inQuotes = false;
+                    }
+                    else
+                    {
+                        inQuotes = true;
+                        if (lastChar == '"')
+                            sb.Append('"');
+                    }
+                }
+                else if (c == ';' && !inQuotes)
+                {
+                    yield return sb.ToString();
+                    sb.Clear();
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+
+                lastChar = c;
+            }
+
+            if (inQuotes)
+                throw new InvalidOperationException("Start quote without end quote");
+
+            yield return sb.ToString();
+        }
+
+        private static void WriteCsv(string fileName, List<List<string>> lines)
+        {
+            using (var writer = File.CreateText(fileName))
+            {
+                foreach (var line in lines)
+                {
+                    bool hadOne = false;
+
+                    foreach (string part in line)
+                    {
+                        if (hadOne)
+                            writer.Write(',');
+                        else
+                            hadOne = true;
+
+                        writer.Write('"');
+                        writer.Write(part.Replace("\"", "\"\""));
+                        writer.Write('"');
+                    }
+
+                    writer.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Cashflow ING Spaar CSV Conversie
+
+Cashflow heeft geen eigen ondersteuning voor het Puntcommagescheiden CSV formaat van de ING, en ING spaarrekeningen kunnen alleen maar geëxporteerd worden in dit formaat.
+
+Deze applicatie converteert een export van het Puntcommagescheiden CSV formaat naar het Kommagescheiden CSV formaat.
+
+Instructies:
+
+* Download the applicatie naar b.v. je Downloads directorie. Dit geeft een applicatie genaamd "CashflowINGSpaarCSVConversie.exe".
+* Download het ING Puntcommagescheiden bestand voor je spaarrekening.
+* Sleep dit gedownloade bestand op het programma.
+
+Dit vervangt het bestand door een bestand met het juiste formaat, dat Cashflow wel kan importeren.
